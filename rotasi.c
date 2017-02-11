b293b894690193caa372c5d99e5afcc1e480734e
scarlettajulia <scarlettajulia@students.itb.ac.id>
2017-02-11 18:46:26 +0700

    implementasi fungsi rotasi

---
+++ rotasi.c
@@ -0,0 +1,29 @@
+#include "rotasi.h"
+#include <math.h>
+
+
+
+titik rotasi (titik p0, titik p1, int d) {
+//rotasi p1 terhadap pivot p0 sebesar d derajat
+	titik p;
+	double a = 0.017444;
+	double sind = sin(d*a);
+	double cosd = cos(d*a);
+	
+	p.x = ((p1.x-p0.x) * cosd ) - ((p1.y-p0.y) * sind) + p0.x;
+	p.y = ((p1.x-p0.x) * sind ) - ((p1.y-p0.y) * cosd) + p0.y;
+	
+	return p;
+	
+}
+
+titik* rotasibanyak (titik p0, titik* p1, int d, int l) {
+//rotasi titik-titik pada array p1 dengan panjang l terhadap pivot p0 sebesar d derajat
+	titik p[l];
+	int i;
+	for (i=0; i<l; l++) {
+		p[i] = rotasi(p0, p1[i], d);
+	}
+	return p;
+}
+
