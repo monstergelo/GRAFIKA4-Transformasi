fc7200d0b2db74dc2b00899802dee5ecfd043289
scarlettajulia <scarlettajulia@students.itb.ac.id>
2017-02-11 18:46:53 +0700

    update header rotasi

--- rotasi.h
+++ rotasi.h
@@ -1,4 +1,6 @@
 #include "gambar.h"
+titik rotasi (titik p0, titik p1, int d);
+//rotasi p1 terhadap pivot p0 sebesar d derajat
 
-void rotasi (titik p1,  titik p2);
-void rotasibanyak (titik p1, titik* p2);
+titik* rotasibanyak (titik p0, titik* p1, int d, int l);
+//rotasi titik-titik pada array p1 dengan panjang l terhadap pivot p0 sebesar d derajat
